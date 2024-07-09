d8e91e81dbce6f425a31841185aba9960ae203d4
Vigneshwaran T <vigneshwarant54@gmail.com>
2024-07-09 21:31:25 +0530

    remove unwanted code

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -17,22 +17,10 @@ export function Header(){
         }, 1000)
     })
 
-
-    // function clickMe(){
-    //     let dateTime = new Date();
-    //     dateTime = moment(dateTime).format('DD-MM-YYYY HH:mm:ss')
-    //     document.getElementById('dateTime').innerHTML = dateTime;
-    // }
-
     return(
         <div component="header">
             <h2 className="mt-4 text-primary border border-primary rounded d-inline-block p-3">Hello this is React Styling</h2>
             <br />
-            {/* <div className="d-flex justify-content-center button">
-                <button type="button" onClick={clickMe} className="btn btn-primary text-align-center" style={{display: 'block',  textAlign:"center"}}>
-                    Click Me to Check Date & Time
-                </button>
-            </div> */}
             <h3 id="" className={styles.result}>
             {clocks}
             </h3>
